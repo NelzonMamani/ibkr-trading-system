7c050d52654794cbbcc6145b47fbde050cd6bad2
NelzonMamani <77867258+NelzonMamani@users.noreply.github.com>
2026-03-26 19:55:57 +0000

    Add strategy-agnostic SetupEngine translation layer

--- src/core/engines/__init__.py
+++ src/core/engines/__init__.py
@@ -2,5 +2,6 @@
 
 from src.core.engines.level_engine import LevelEngine
 from src.core.engines.structure_engine import StructureEngine
+from src.core.engines.setup_engine import SetupEngine
 
-__all__ = ["LevelEngine", "StructureEngine"]
+__all__ = ["LevelEngine", "StructureEngine", "SetupEngine"]
